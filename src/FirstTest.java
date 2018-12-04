0bdd3890c490131a601539f3ca9bf2355faea246
Jane Rozental <rozental.jane@gmail.com>
2018-12-03 21:14:22 -0600

    Homework # 4

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,24 +1,26 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.List;
 
 public class FirstTest {
     private AppiumDriver driver;
 
 
     @Before
-    public void setUp() throws Exception
-    {
+    public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         capabilities.setCapability("platformName", "Android");
@@ -38,8 +40,7 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest()
-    {
+    public void firstTest() {
 
         waitForElementAndClick(
                 By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
@@ -62,8 +63,7 @@ public class FirstTest {
     }
 
     @Test
-    public void testCancelSearch()
-    {
+    public void testCancelSearch() {
         waitForElementAndClick(
                 By.id("org.wikipedia:id/search_container"),
                 "Cannot find 'Search Wikipedia' input",
@@ -85,11 +85,11 @@ public class FirstTest {
         );
 
 
-       waitForElementAndClick(
-               By.id("org.wikipedia:id/search_close_btn"),
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
                 "Cannot find X to cancel search",
                 5
-       );
+        );
 
         waitForElementNotPresent(
                 By.id("org.wikipedia:id/search_close_btn"),
@@ -138,81 +138,139 @@ public class FirstTest {
 
     }
 
-        @Test
-        public void testSearchText()
-        {
-            waitForElementAndClick(
-                    By.id("org.wikipedia:id/search_container"),
-                    "Cannot find search field",
-                    5
-            );
+    @Test
+    public void testSearchText() {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find search field",
+                5
+        );
 
-            WebElement search = waitForElementPresent(
-                    By.id("org.wikipedia:id/search_src_text"),
-                    "Cannot find 'Search…' in search field",
-                    5
-            );
+        WebElement search = waitForElementPresent(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot find 'Search…' in search field",
+                5
+        );
 
-            String search_text = search.getAttribute("text");
+        String search_text = search.getAttribute("text");
 
-            Assert.assertEquals(
-                    "We see unexpected word!",
-                    "Search…",
-                    search_text
-            );
-        }
+        Assert.assertEquals(
+                "We see unexpected word!",
+                "Search…",
+                search_text
+        );
+    }
 
-        @Test
-        public void testArticleFound()
-        {
-            waitForElementAndClick(
-                    By.id("org.wikipedia:id/search_container"),
-                    "Cannot find 'Search Wikipedia' input",
-                    5
-            );
+     @Test
+    public void testNumberOfSearchResults() {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
 
-            waitForElementAndSendKeys(
-                    By.xpath("//*[contains(@text, 'Search…')]"),
-                    "Java",
-                    "Cannot find search input",
-                    5
-            );
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
 
-            WebElement article_element = waitForElementPresent(
-                   By.id("org.wikipedia:id/page_list_item_title"),
-                   "Cannot find article title",
-                    15
+       int countOfElements = waitForElementAndCount(
+                By.id("org.wikipedia:id/page_list_item_container"),
+                "Cannot find more than few results",
+                5
+        );
 
-            );
+       if (countOfElements < 2)
+       {
+           Assert.fail("Count of articles was less than 2");
+       }
 
-          String article_title = article_element.getAttribute("text");
-
-           Assert.assertEquals(
-                   "We see unexpected title!",
-                    "Java",
-                    article_title
-            );
-
-
-           waitForElementAndClear(
-                    By.id("org.wikipedia:id/search_src_text"),
-                    "Cannot find search field",
-                    5
-            );
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot find search field",
+                5
+        );
 
 
-            waitForElementAndClick(
-                    By.id("org.wikipedia:id/search_close_btn"),
-                    "Cannot find X to cancel search",
-                    5
-            );
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "Cannot find X to cancel search",
+                5
+        );
 
-            waitForElementNotPresent(
-                    By.id("org.wikipedia:id/search_close_btn"),
-                    "X is still present on the page",
-                    5
-            );
-        }
+        waitForElementNotPresent(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "X is still present on the page",
+                5
+        );
+    }
+
+
+     @Test
+    public void testSearchWordIsPresent() {
+
+         waitForElementAndClick(
+                 By.id("org.wikipedia:id/search_container"),
+                 "Cannot find 'Search Wikipedia' input",
+                 5
+         );
+
+         waitForElementAndSendKeys(
+                 By.xpath("//*[contains(@text, 'Search…')]"),
+                 "Selenium",
+                 "Cannot find search input",
+                 5
+         );
+
+         boolean textFound =  waitForElementAndCheckText(
+                 By.id("org.wikipedia:id/page_list_item_title"),
+                 "Selenium",
+                 "No search results",
+                 5
+         );
+
+         Assert.assertTrue("Search result does not contain desired word",textFound);
+
+    }
+
+     @Test
+    public void testSwipeArticleTitle() {
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find Search Wikipedia input",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Appium",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Appium']"),
+                "Cannot find 'Appium' in search",
+                5
+        );
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                15
+
+        );
+
+        swipeUpToFindElement(
+                By.xpath("//*[@text='View page in browser']"),
+                "Test error message"
+        );
+
+
+    }
 
 
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
@@ -225,8 +283,7 @@ public class FirstTest {
     }
 
 
-    private WebElement waitForElementPresent(By by, String error_message)
-    {
+    private WebElement waitForElementPresent(By by, String error_message) {
         return waitForElementPresent(by, error_message, 5);
     }
 
@@ -255,12 +312,59 @@ public class FirstTest {
         );
     }
 
+
     private WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds)
     {
         WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
         element.clear();
         return element;
     }
+
+
+    private int waitForElementAndCount(By by, String error_message, long timeoutInSeconds)
+    {
+        waitForElementPresent(by,error_message,timeoutInSeconds);
+        int count = driver.findElements(by).size();
+        return count;
+    }
+
+    private boolean waitForElementAndCheckText(By by, String containsString, String error_message, long timeoutInSeconds)
+    {
+        List<WebElement> elements = driver.findElements(by);
+        for (WebElement element: elements) {
+            if (!element.getText().contains(containsString))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected void swipeUp(int timeOfSwipe)
+    {
+       TouchAction action = new TouchAction(driver);
+        Dimension size = driver.manage().window().getSize();
+        int x = size.width / 2;
+        int start_y = (int) (size.height * 0.8);
+        int end_y = (int) (size.height * 0.2);
+
+       action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).release().perform();
+
+    }
+
+    protected void swipeUpQuick()
+    {
+        swipeUp(200);
+    }
+
+    protected void swipeUpToFindElement(By by, String error_message)
+    {
+        while (driver.findElements(by).size() == 0){
+            swipeUpQuick();
+        }
+    }
+
+
 
 }
 
